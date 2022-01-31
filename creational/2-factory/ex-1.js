452bd689972f6248564feae5db9f1d1fbabcc977
Thanh Nguyen <vnscriptkid@gmail.com>
2022-01-31 16:44:07 +0700

    feat: factory ex 1

---
+++ creational/2-factory/ex-1.js
@@ -0,0 +1,34 @@
+// why, when
+// diff btw factory and builder
+// variations?
+
+CoordinateSystem = {
+  CARTESIAN: 0,
+  POLAR: 1,
+};
+
+class Point {
+  // Original API:
+  // constructor(x, y) {
+  //     this.x = x;
+  //     this.y = y;
+  // }
+
+  // Modification to support polar point
+  constructor(a, b, cs = CoordinateSystem.CARTESIAN) {
+    switch (cs) {
+      case CoordinateSystem.CARTESIAN:
+        this.x = a;
+        this.y = b;
+        break;
+      case CoordinateSystem.POLAR:
+        this.x = a * Math.cos(b);
+        this.y = a * Math.sin(b);
+        break;
+    }
+  }
+}
+
+// expected api
+Point.newCartesianPoint(2, 3);
+Point.newPolarPoint(5, Math.PI / 2);
