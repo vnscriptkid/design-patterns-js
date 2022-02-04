a68125bad70fd125e0f1587e9eea046af18187b4
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-04 13:56:59 +0700

    feat: adapter submit 1,2

---
+++ creational/5-adapter/submit-2.js
@@ -0,0 +1,127 @@
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return `(${this.x}, ${this.y})`;
+  }
+}
+
+class Line {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  toString() {
+    return `${this.start.toString()}→${this.end.toString()}`;
+  }
+}
+
+class VectorObject extends Array {}
+
+class VectorRectangle extends VectorObject {
+  constructor(x, y, width, height) {
+    super();
+    this.push(new Line(new Point(x, y), new Point(x + width, y)));
+    this.push(
+      new Line(new Point(x + width, y), new Point(x + width, y + height))
+    );
+    this.push(new Line(new Point(x, y), new Point(x, y + height)));
+    this.push(
+      new Line(new Point(x, y + height), new Point(x + width, y + height))
+    );
+    this.push;
+  }
+}
+
+// ↑↑↑ this is your API ↑↑↑
+
+// ↓↓↓ this is what you have to work with ↓↓↓
+String.prototype.hashCode = function () {
+  if (Array.prototype.reduce) {
+    return this.split("").reduce(function (a, b) {
+      a = (a << 5) - a + b.charCodeAt(0);
+      return a & a;
+    }, 0);
+  }
+  let hash = 0;
+  if (this.length === 0) return hash;
+  for (let i = 0; i < this.length; i++) {
+    const character = this.charCodeAt(i);
+    hash = (hash << 5) - hash + character;
+    hash = hash & hash; // Convert to 32-bit integer
+  }
+  return hash;
+};
+
+// todo:
+
+class LineToPointAdapter extends Array {
+  static cache = {};
+
+  constructor(line) {
+    super();
+
+    // todo: gen hash, check in cache?
+    this.hash = line.toString().hashCode();
+
+    if (!(this.hash in LineToPointAdapter.cache)) {
+      console.log(
+        `${LineToPointAdapter.count++}: Generating ` +
+          `points for line ${line.toString()} (with caching)`
+      );
+      // how to share cache data accross object instances: static member of class
+      // how to share count data accross object instances: static member of class
+
+      let points = [];
+
+      let left = Math.min(line.start.x, line.end.x);
+      let right = Math.max(line.start.x, line.end.x);
+      let top = Math.min(line.start.y, line.end.y);
+      let bottom = Math.max(line.start.y, line.end.y);
+
+      if (right - left === 0) {
+        for (let y = top; y <= bottom; ++y) {
+          points.push(new Point(left, y));
+        }
+      } else if (line.end.y - line.start.y === 0) {
+        for (let x = left; x <= right; ++x) {
+          points.push(new Point(x, top));
+        }
+      }
+
+      // todo: save points to cache
+      LineToPointAdapter.cache[this.hash] = points;
+    }
+  }
+
+  get items() {
+    return LineToPointAdapter.cache[this.hash];
+  }
+}
+LineToPointAdapter.count = 0;
+LineToPointAdapter.cache = {};
+
+let vectorObjects = [
+  new VectorRectangle(1, 1, 10, 10),
+  new VectorRectangle(3, 3, 6, 6),
+];
+
+let drawPoint = function (point) {
+  process.stdout.write(".");
+};
+
+let draw = function () {
+  for (let vo of vectorObjects) {
+    for (let line of vo) {
+      let adapter = new LineToPointAdapter(line);
+      adapter.items.forEach(drawPoint);
+    }
+  }
+};
+
+draw();
+draw();
