f7e06e253316ad98d45109d9e640702a7d089db2
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-09 22:10:22 +0700

    feat: proxy ex 4

---
+++ structural/7-proxy/ex-4.js
@@ -0,0 +1,26 @@
+class Image {
+  constructor(url) {
+    // problem: loading image when a new image instance is initialized
+    this.url = url;
+    console.log(`Loading image from ${this.url}`);
+  }
+
+  draw() {
+    console.log(`Drawing image ${this.url}`);
+  }
+}
+
+// your boss comes in and ask you to optimize this
+// without modifying existing code.
+class LazyImage {
+  // todo: delay loading at the time of drawing
+}
+
+function drawImage(img) {
+  console.log("About to draw the image");
+  img.draw();
+  console.log("Done drawing the image");
+}
+
+let img = new LazyImage("http://pokemon.com/pikachu.png");
+drawImage(img);
