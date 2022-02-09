fc032cd8c602a9e2009c8a02389add3deebe4e8b
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-09 21:58:11 +0700

    feat: proxy ex 3

---
+++ structural/7-proxy/ex-3.js
@@ -0,0 +1,29 @@
+class Car {
+  drive() {
+    console.log("Car being driven");
+  }
+}
+
+class Driver {
+  constructor(age) {
+    this.age = age;
+  }
+}
+
+let car = new Car();
+car.drive();
+
+// problem: ppl under 18 years old should not be allowed to drive.
+// todo: add a proxy layer over driver and car
+
+class CarProxy {
+  constructor(driver) {
+    this.driver = driver;
+    this._car = new Car();
+  }
+
+  // todo: preserve the interface of car
+}
+
+let car2 = new CarProxy(new Driver(12)); // try 22
+car2.drive();
