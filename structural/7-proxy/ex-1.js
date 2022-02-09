0069df9450aa61b715714349a557db3e94e818eb
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-09 21:33:08 +0700

    feat: proxy ex 1

---
+++ structural/7-proxy/ex-1.js
@@ -0,0 +1,15 @@
+// this proxy class represents a concept, for example 5% but in calculation we want 0.05 instead
+class Percentage {
+  constructor(percent) {
+    this.percent = percent; // 0 to 100
+  }
+
+  toString() {
+    return `${this.percent}%`;
+  }
+
+  // todo: define method that gets actual number used in calculation
+}
+
+let fivePercent = new Percentage(5);
+console.log(`${fivePercent} of 50 is ${50 * fivePercent}`);
