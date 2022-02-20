b7f41afa769034812af670d9113f80328b83a0be
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-20 14:28:14 +0700

    feat: visitor ex 1

---
+++ behavioral/11-visitor/ex-1.js
@@ -0,0 +1,35 @@
+/* Intrusive visitor */
+
+class NumberExpression {
+  constructor(value) {
+    this.value = value;
+  }
+
+  // buffer visits numberExpression
+  print(buffer) {
+    // todo: build up the buffer
+  }
+}
+
+class AdditionExpression {
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  // buffer visits additionExpression
+  print(buffer) {
+    // todo: build up the buffer
+    // (1+2)
+  }
+}
+
+// 1 + (2+3)
+let e = new AdditionExpression(
+  new NumberExpression(1),
+  new AdditionExpression(new NumberExpression(2), new NumberExpression(3))
+);
+// create a visitor, passing it to print, so that visitor can go recursively and visits all expressions
+let buffer = [];
+e.print(buffer);
+console.log(buffer.join(""));
